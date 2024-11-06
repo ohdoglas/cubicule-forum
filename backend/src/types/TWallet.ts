3c6c58e257c0568f62b57d99358a8ccce00dcda4
ohdoglas <dughlaisdd@gmail.com>
2024-11-06 17:40:32 -0300

    change: minor change on TWallet type

--- backend/src/types/TWallet.ts
+++ backend/src/types/TWallet.ts
@@ -2,8 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 type TWallet = {
-    id?: ReturnType<typeof uuidv4>;
-    user_id?: ReturnType<typeof uuidv4>;
+    id: ReturnType<typeof uuidv4>;
+    user_id: ReturnType<typeof uuidv4>;
     balance: number;
     created_at: Date;
     updated_at?: Date;
