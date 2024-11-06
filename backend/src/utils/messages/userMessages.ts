9603d2ac63a6eabf1042a5fe1a71af87b29d3e31
ohdoglas <dughlaisdd@gmail.com>
2024-11-06 17:39:16 -0300

    feature: new user messages

---
+++ backend/src/utils/messages/userMessages.ts
@@ -0,0 +1,19 @@
+const USER = {
+    ERR: {
+        INVALID_USERNAME: "Please enter a username with a valid format.",
+        INVALID_EMAIL: "Please enter an email with a valid format.",
+        WEAK_PASSWORD:  `Weak Password!
+        Password must contain the following requirements:
+        Contains at least one capital letter.
+        Contains at least one number.
+        Contains at least one special character.
+        It is at least 6 characters long.`,
+        NOT_UNIQUE_USERNAME: "The entered user is already in use.",
+        NOT_UNIQUE_EMAIL: "The email entered is already in use",
+    },
+    SUCCESS: {
+
+    }
+}
+
+export default USER;
