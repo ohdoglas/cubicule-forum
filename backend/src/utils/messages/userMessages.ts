c48464a18f6c6ed2df002103ab124026ec41428a
ohdoglas <dughlaisdd@gmail.com>
2024-12-06 07:05:27 -0300

    update: new user messages: email confirmation messages

--- backend/src/utils/messages/userMessages.ts
+++ backend/src/utils/messages/userMessages.ts
@@ -12,9 +12,14 @@ const USER = {
         NOT_UNIQUE_USERNAME: "The entered user is already in use.",
         NOT_UNIQUE_EMAIL: "The email entered is already in use",
         USER_NOT_FOUND: "User not found. Please check your credentials and try again.",
+        MISSING_EMAIL_CONFIRM_TOKEN: `A token is required to confirm your email. Please use the link sent to your email, which looks like this: "https:ourdomain/token"`,
+        INVALID_CONFIRMATION_TOKEN: `The provided token is invalid or does not correspond to any registered user.
+        A token is required to confirm your email. Please use the link sent to your email, which looks like this:
+        "https:ourdomain/token".`,
+        ALREADY_VERIFIED: 'It seems that your email has already been confirmed. If you have any questions or need further assistance, please feel free to reach out to our support team.',
     },
     SUCCESS: {
-
+        EMAIL_CONFIRMATION: 'Email confirmation completed successfully! You can now access your account.',
     }
 }
 
