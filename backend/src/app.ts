1ac4478fe012e269fe57d5ab505a5bce03484046
ohdoglas <dughlaisdd@gmail.com>
2024-12-09 11:52:43 -0300

    feature: initial seed role and permission logic implemented

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,32 @@ import 'dotenv/config';
 import InitialSetup from "./models/initialSetup";
 import sendSetupMail from "./utils/security/setupMail";
 import SETUP from "./utils/messages/setupMessages";
+import Role from "./models/roles";
+import Permission from "./models/permissions";
 
 const adminMail = process.env.ADMIN_MAIL;
 const app: Application = express();
 
 app.use(express.json());
+
+async function validateEnv() {
+    if (!adminMail) {
+        throw new Error(SETUP.ERR.MISSING_MAIL_ENV);
+    }
+}
+
+async function seedDataBase() {
+    try {
+        const createdRoles = await Role.seed();
+        const createdPermissions = await Permission.seed();
+
+        console.log(`Roles created: ${createdRoles}`);
+        console.log(`Permissions created: ${createdPermissions}`);
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
 
 async function checkInitialSetup() {
     try {
@@ -25,12 +46,23 @@ async function checkInitialSetup() {
         }
     } catch (error) {
         console.error(SETUP.ERR.SEND_EMAIL_FAILED, error);
+        throw error;
     }
-
 }
 
-checkInitialSetup().catch(console.error);
+(async () => {
+    try {
+        await validateEnv();
+        await seedDataBase();
+        await checkInitialSetup();
 
-app.use(routes);
+        app.use(routes);
+
+
+    } catch (error) {
+        console.error(SETUP.ERR.SEND_EMAIL_FAILED, error);
+        throw error;
+    }
+})();
 
 export default app;
