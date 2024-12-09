5d54fdf0a8522597824f0a3e82552253d6eecc98
ohdoglas <dughlaisdd@gmail.com>
2024-12-09 11:50:59 -0300

    changes: minor changes to assign roles on user controller

--- backend/src/controllers/userControllers/register.ts
+++ backend/src/controllers/userControllers/register.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import SERVER from "../../utils/messages/serverMessages";
 import User from "../../models/user";
 import USER from "../../utils/messages/userMessages";
+import Role from "../../models/roles";
+import Permission from "../../models/permissions";
+import { Permissions, Roles } from "../../utils/enums/accessEnums";
 
 
 
@@ -10,11 +13,15 @@ export default class userRegistration {
         const { username, email, password } = req.body;
 
         try {
-            await User.create({
+            const newUser = await User.create({
                 username,
                 email,
                 password_hash: password
             })
+
+            await Role.assignUserRole(Roles.USER, newUser.id);
+            await Permission.assignPermissions(Permissions.READ_ONLY, newUser.id);
+            await Permission.assignPermissions(Permissions.VIEW_DASHBOARD, newUser.id);
 
             return res.status(201).json({
                 message: USER.SUCCESS.CONFIRM_EMAIL_SENT
