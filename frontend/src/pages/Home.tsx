8feb9534947f2f898534e0be95ae1efed00d06db
ohdoglas <dughlaisdd@gmail.com>
2024-12-14 11:49:50 -0300

    update/changes: homepage html/css minor changes and fixes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,42 +9,50 @@ const Home: React.FC = () => {
     return (
         <body className="page-one">
             <Header />
-
-        <main className="main_content">
-            <section>
-                <h1 className="welcome">
-                    WELCOME TO CUBICLES.
-                </h1>
-                <p className="welcome_text">
-                A pixel-perfect workspace designed for seamless collaboration
-                and unmatched creativity. Join us and transform the way you work, share, and grow!
-                </p>
-                <button className="get_started_button">
-                    GET STARTED FOR FREE
-                </button>
-            </section>
-            <section>
-                <h1 className="intro">
-                    COLLABORATE.
-                    CREATE.
-                    ACHIEVE.
-                </h1>
-                <p className="intro_text">
-                Cubicles is more than just a platform—it’s your digital workspace revolution. From real-time collaboration tools to intuitive task management, we bring teams together to build ideas and make them thrive. Designed for innovators, creators, and doers, Cubicles empowers you to work smarter, faster, and with style.
-                </p>
-                <h1 className="features">
-                What Makes Cubicles Different?
-                </h1>
-                <Link to={"/features"}>
-                    <button className="features_button">
-                        discover our features
-                    </button>
-                </Link>
-            </section>
-        </main>
-        <Footer />
-
-
+            <main className="main_content">
+                <section>
+                    <h1 className="welcome">
+                        WELCOME TO CUBICLES.
+                    </h1>
+                    <p className="welcome_text">
+                        A pixel-perfect workspace designed for seamless collaboration
+                        and unmatched creativity.
+                    </p>
+                    <p className="welcome_text">
+                        Join us and transform the way you work, share, and grow! <br />
+                        <Link to={"/register"}>
+                            <button className="get_started_button">
+                                GET STARTED FOR FREE
+                            </button>
+                        </Link>
+                    </p>
+                </section>
+                <section>
+                    <h1 className="intro">
+                        COLLABORATE.
+                        CREATE.
+                        ACHIEVE.
+                    </h1>
+                    <p className="intro_text">
+                        Cubicles is more than just a platform—it’s your digital workspace revolution.
+                    </p>
+                    <p className="intro_text">
+                        From real-time collaboration tools to intuitive task management, we bring teams together to build ideas and make them thrive.
+                    </p>
+                    <p className="intro_text">
+                        Designed for innovators, creators, and doers, Cubicles empowers you to work smarter, faster, and with style.
+                    </p>
+                    <h1 className="features">
+                        What Makes <br />Cubicles Different? <br />
+                        <Link to={"/features"}>
+                            <button className="features_button">
+                                discover our features
+                            </button>
+                        </Link>
+                    </h1>
+                </section>
+            </main>
+            <Footer />
         </body>
     )
 }
