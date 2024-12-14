de8ee33674d68a6e5aa9418654b43d4be3d03426
ohdoglas <dughlaisdd@gmail.com>
2024-12-14 12:32:04 -0300

    update/changes: features page minor html/css changes and fixes

--- frontend/src/pages/Features.tsx
+++ frontend/src/pages/Features.tsx
@@ -16,63 +16,66 @@ const Features: React.FC = () => {
                     What Makes Cubicles Different?
                 </h1>
                 <p className="feature_text">
-                Pixel-Perfect Collaboration:
-                “Connect and share ideas effortlessly in real-time with pixel-inspired tools that blend creativity and functionality.”
+                    Pixel-Perfect Collaboration: <br />
+                    “Connect and share ideas effortlessly in real-time with pixel-inspired tools that blend creativity and functionality.”
                 </p>
                 <p className="feature_text">
-                Customizable Workspaces:
-                “Personalize your digital cubicle with themes, layouts, and widgets to make it truly yours.”
+                    Customizable Workspaces: <br />
+                    “Personalize your digital cubicle with themes, layouts, and widgets to make it truly yours.”
                 </p>
                 <p className="feature_text">
-                Your data, your rules. Cubicles ensures the highest level of encryption and control for your team’s privacy.
+                    Your data, your rules. Cubicles ensures the highest level of encryption and control for your team’s privacy. <br />
+                    <Link to={"/register"}>
+                        <button className="get_started_button">
+                            GET STARTED FOR FREE
+                        </button>
+                    </Link>
                 </p>
 
-                <button className="get_started_button">
-                    GET STARTED FOR FREE
-                </button>
             </section>
             <section className="section_two">
-            <h1 className="feature_title">
-                Customizable Virtual Cubicles
+                <h1 className="feature_title">
+                    Customizable Virtual Cubicles
                 </h1>
                 <p className="feature_text">
-                Your Cubicle is your personal space in the digital world!
-                Decorate it with unique items, NFTs, and collectibles to showcase your personality. Use exclusive designs and layouts to transform your cubicle into a hub that reflects your style, achievements, and creativity.
+                    Your Cubicle is your personal space in the digital world! <br />
+                    Decorate it with unique items, NFTs, and collectibles to showcase your personality. Use exclusive designs and layouts to transform your cubicle into a hub that reflects your style, achievements, and creativity.
                 </p>
             </section>
             <section className="section_three">
-            <h1 className="feature_title">
-                Dynamic Coin Generation
+                <h1 className="feature_title">
+                    Dynamic Coin Generation
                 </h1>
                 <p className="feature_text">
-                Earn coins through a fun and interactive clicker-style mechanic. Whether you're exploring, engaging with the community, or completing tasks, every action helps you generate currency. The more active you are, the more you'll earn!
+                    Earn coins through a fun and interactive clicker-style mechanic. Whether you're exploring, engaging with the community, or completing tasks, every action helps you generate currency. The more active you are, the more you'll earn!
                 </p>
             </section>
             <section className="section_four">
-            <h1 className="feature_title">
-                Integrated Marketplace
+                <h1 className="feature_title">
+                    Integrated Marketplace
                 </h1>
                 <p className="feature_text">
-                The NFT Marketplace is your destination for trading unique digital assets.
-                Buy and sell rare items or unlock special packs using your earned coins.
-                Discover exclusive designs to enhance your cubicle and stand out in the community.
-                Coins act as the lifeblood of the marketplace, offering seamless access to premium features without real-money investment.
+                    The NFT Marketplace is your destination for trading unique digital assets.
+                    Buy and sell rare items or unlock special packs using your earned coins.
+                    Discover exclusive designs to enhance your cubicle and stand out in the community.
+                    Coins act as the lifeblood of the marketplace, offering seamless access to premium features without real-money investment.
                 </p>
             </section>
             <section className="section_five">
-            <h1 className="feature_title">
-                Cubicles as Interactive Spaces
+                <h1 className="feature_title">
+                    Cubicles as Interactive Spaces
                 </h1>
                 <p className="feature_text">
-                Cubicles aren’t just static displays—they’re interactive zones!
-                Host conversations or events directly in your space.
-                Integrate music players, animations, and community features to create a lively atmosphere.
-                Allow visitors to explore your curated NFT collection or engage in real-time chats.
+                    Cubicles aren’t just static displays—they’re interactive zones! <br />
+                    Host conversations or events directly in your space. <br />
+                    Integrate music players, animations, and community features to create a lively atmosphere. <br />
+                    Allow visitors to explore your curated NFT collection or engage in real-time chats. <br />
+                    <Link to={"/register"}>
+                    <button className="get_started_button">
+                        GET STARTED FOR FREE
+                    </button>
+                    </Link>
                 </p>
-
-                <button className="get_started_button">
-                    GET STARTED FOR FREE
-                </button>
             </section>
         </main>
         <Footer />
